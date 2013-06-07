1c51392d69a2e38d8a6b40fcb1f1b556daf0a666
Johan Andersson <johan.rejeep@gmail.com>
2013-06-07 16:04:03 +0200

    Add simple support for options.

--- commander.el
+++ commander.el
@@ -1,4 +1,4 @@
-;;; commander.el --- TODO
+;;; commander.el --- Command line parser
 
 ;; Copyright (C) 2013 Johan Andersson
 
@@ -28,6 +28,111 @@
 ;; Boston, MA 02110-1301, USA.
 
 ;;; Code:
+
+(eval-when-compile
+  (require 'cl))
+(require 's)
+(require 'dash)
+
+(defstruct commander-option flag description function required-argument optional-argument default-value)
+(defstruct commander-command command description function)
+
+(defvar commander-options nil)
+(defvar commander-commands nil)
+
+(defconst commander-option-re
+  "\\(-[A-Za-z0-9-]\\|--[A-Za-z0-9][A-Za-z0-9-]+\\)"
+  "Regex matching an option flag.")
+
+(defun commander--option (flags description function &optional default-value)
+  (let ((required-argument) (optional-argument))
+    (-map
+     (lambda (flag)
+       (let ((matches (s-match (concat "^" commander-option-re " " "<\\([a-z]+\\)>" "$") flag)))
+         (when matches
+           (setq flag (nth 1 matches))
+           (setq required-argument (nth 2 matches))))
+       (let ((matches (s-match (concat "^" commander-option-re " " "\\[\\([a-z]+\\)\\]" "$") flag)))
+         (when matches
+           (setq flag (nth 1 matches))
+           (setq optional-argument (nth 2 matches))))
+       (add-to-list
+        'commander-options
+        (make-commander-option
+         :flag flag
+         :description description
+         :function function
+         :required-argument required-argument
+         :optional-argument optional-argument
+         :default-value default-value)))
+     (-map 's-trim (s-split "," flags)))))
+
+(defun commander--command (command description function)
+  (add-to-list
+   'commander-commands
+   (make-commander-command
+    :command command
+    :description description
+    :function function)))
+
+(defun commander--find-option (option)
+  (-first
+   (lambda (commander-option)
+     (equal (commander-option-flag commander-option) option))
+   commander-options))
+
+(defun commander--handle-options (arguments)
+  (let ((i 0) (rest))
+    (while (< i (length arguments))
+      (let ((argument (nth i arguments)))
+        (if (s-matches? (concat "^" commander-option-re "$") argument)
+            (let ((commander-option (commander--find-option argument)))
+              (cond (commander-option
+                     (let ((function (commander-option-function commander-option)))
+                       (cond ((commander-option-required-argument commander-option)
+                              (setq i (1+ i))
+                              (let ((next-argument (nth i arguments)))
+                                (if next-argument
+                                    (funcall function next-argument)
+                                  (error "Option `%s` requires argument" argument))))
+                             ((commander-option-optional-argument commander-option)
+                              (let ((next-argument (nth (1+ i) arguments)))
+                                (if (or (not next-argument) (s-matches? commander-option-re next-argument))
+                                    (setq next-argument (commander-option-default-value commander-option))
+                                  (setq i (1+ i)))
+                                (funcall function next-argument)))
+                             (t (funcall function)))))
+                    (t (error "Option `%s` not available" argument))))
+          (add-to-list 'rest argument)))
+      (setq i (1+ i)))
+    rest))
+
+(defun commander--parse (arguments)
+  (let ((rest (commander--handle-options arguments)))
+    ;; TODO: Handle rest
+    )
+  )
+
+(defmacro commander (&rest forms)
+  "Specify option/command schema."
+  `(funcall 'commander--do (quote ,forms)))
+
+(defun commander--do (forms)
+  (setq commander-options nil)
+  (setq commander-commands nil)
+
+  (dolist (form forms)
+    ;; cl-flet
+    (pcase form
+      (`(option ,flags ,description ,function)
+       (commander--option flags description function))
+      (`(option ,flags ,description ,function ,default-value)
+       (commander--option flags description function default-value))
+      (`(command ,command ,description ,function)
+       (commander--command command description function))
+      (`(parse ,arguments)
+       (commander--parse arguments))
+      (_ (error "Invalid commander directive: %s" form)))))
 
 (provide 'commander)
 
--- test/commander-test.el
+++ test/commander-test.el
@@ -1,1 +1,112 @@
 (require 'ert)
+(require 'el-mock)
+(require 'commander (expand-file-name "commander.el" (directory-file-name default-directory)))
+
+(ert-deftest test-commander-option-present-long ()
+  (with-mock
+   (mock (help) :times 1)
+   (commander
+    (option "--help" "HELP" help)
+    (parse ("--help")))))
+
+(ert-deftest test-commander-option-present-short ()
+  (with-mock
+   (mock (help) :times 1)
+   (commander
+    (option "-h" "HELP" help)
+    (parse ("-h")))))
+
+(ert-deftest test-commander-option-present-with-others ()
+  (with-mock
+   (mock (help) :times 1)
+   (commander
+    (option "--before" "BEFORE" ignore)
+    (option "--after" "AFTER" ignore)
+    (option "--help" "HELP" help)
+    (parse ("--before" "--help" "--after")))))
+
+(ert-deftest test-commander-option-alias ()
+  (with-mock
+   (mock (help) :times 2)
+   (commander
+    (option "--help, -h" "HELP" help)
+    (parse ("--help"))
+    (parse ("-h")))))
+
+(ert-deftest test-commander-option-none ()
+  (with-mock
+   (not-called help)
+   (commander
+    (option "--help" "HELP" help)
+    (parse nil))))
+
+(ert-deftest test-commander-option-not-present ()
+  (with-mock
+   (mock (error "Option `%s` not available" "--do-not-help"))
+   (commander
+    (option "--help" "HELP" help)
+    (parse ("--do-not-help")))))
+
+(ert-deftest test-commander-required-argument-present ()
+  (with-mock
+   (mock (help "command") :times 1)
+   (commander
+    (option "--help <command>" "COMMAND HELP" help)
+    (parse ("--help" "command")))))
+
+(ert-deftest test-commander-required-argument-present-with-friends ()
+  (with-mock
+   (mock (help "command") :times 1)
+   (commander
+    (option "--help <command>" "COMMAND HELP" help)
+    (option "--before" "BEFORE" ignore)
+    (option "--after" "AFTER" ignore)
+    (parse ("--before" "--help" "command" "--after")))))
+
+(ert-deftest test-commander-required-argument-not-present-next-is-option ()
+  (with-mock
+   (mock (help "--command") :times 1)
+   (commander
+    (option "--help <command>" "COMMAND HELP" help)
+    (parse ("--help" "--command")))))
+
+(ert-deftest test-commander-required-argument-not-present ()
+  (with-mock
+   (mock (error "Option `%s` requires argument" "--help") :times 1)
+   (commander
+    (option "--help <command>" "COMMAND HELP" help)
+    (parse ("--help")))))
+
+(ert-deftest test-commander-optional-argument-present ()
+  (with-mock
+   (mock (help "command") :times 1)
+   (commander
+    (option "--help [command]" "COMMAND HELP" help)
+    (parse ("--help" "command")))))
+
+(ert-deftest test-commander-optional-argument-not-present ()
+  (with-mock
+   (mock (help) :times 1)
+   (commander
+    (option "--help [command]" "COMMAND HELP" help)
+    (parse ("--help")))))
+
+(ert-deftest test-commander-optional-argument-not-present-with-default-value ()
+  (with-mock
+   (mock (help "all") :times 1)
+   (commander
+    (option "--help [command]" "COMMAND HELP" help "all")
+    (parse ("--help")))))
+
+(ert-deftest test-commander-mixed-with-noise ()
+  (with-mock
+   (mock (x) :times 1)
+   (mock (help "bar") :times 1)
+   (mock (show "baz") :times 1)
+   (commander
+    (option "-x" "X" x)
+    (option "--show <command>" "SHOW" show)
+    (option "--help [command]" "HELP" help)
+    (parse ("foo" "-x" "--help" "bar" "--show" "baz" "qux")))))
+
+(ert-run-tests-batch-and-exit)
