81541924a6e9d873b1c2f126fb08244f91c636f0
Johan Andersson <johan.rejeep@gmail.com>
2013-06-10 23:24:48 +0200

    Update command example.

--- examples/command.el
+++ examples/command.el
@@ -1,13 +1,17 @@
-;; carton exec emacs --script examples/commands.el -- sum 1 2 3 4 5 --say Muuuu
+;; carton exec emacs --script examples/commands.el -- sum 1 2 3 4 5 --say One Two Three --four
 
 (require 'commander (expand-file-name "commander.el" default-directory))
 
-(defun say (arg)
-  (message "Saying: %s" arg))
+(defun say (&rest args)
+  (message "Saying: %s" (mapconcat 'identity args " ")))
+
+(defun four ()
+  (say "Four"))
 
 (defun sum (&rest args)
   (message "Sum is: %d" (apply '+ (mapcar 'string-to-int args))))
 
 (commander
  (option "--say <*>" "..." 'say)
+ (option "--four" "..." 'four)
  (command "sum [args]" "..." 'sum 0))
