537768a87062120e6399ca478bb311a50956fe2a
Johan Andersson <johan.rejeep@gmail.com>
2013-06-11 10:28:26 +0200

    Add usage example.

---
+++ examples/usage.el
@@ -0,0 +1,18 @@
+;; carton exec emacs --script examples/usage.el -- --help
+
+(require 'commander (expand-file-name "commander.el" default-directory))
+
+(defun cow (arg)
+  (message "The cow says: %s" arg))
+
+(defun dog (arg)
+  (message "The dog says: %s" arg))
+
+(commander
+ (option "--cow <arg>" "Lorem ipsum dolor sit amet" 'cow)
+ (option "--dog [arg]" "Consectetur adipiscing elit. Donec orci dolor" 'dog "Woff Woff")
+ (option "--help, -h" "Luctus sed ullamcorper nec" 'commander-print-usage)
+ (command "help" "Imperdiet eu eros" 'commander-print-usage)
+ (command "foo" "Duis rhoncus luctus tellus" 'ignore)
+ (command "barbaz" "At porta odio congue id" 'ignore)
+ (command "qux" "Nunc tempus nisl ac erat vulputate vestibulum" 'ignore))
