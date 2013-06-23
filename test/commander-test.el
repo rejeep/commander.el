449d05a5d5ac664990c7dbc84544c3ce5afc7d0d
Johan Andersson <johan.rejeep@gmail.com>
2013-06-23 14:38:10 +0200

    Remove test since those things are tested elsewhere.

--- test/commander-test.el
+++ test/commander-test.el
@@ -1,16 +1,3 @@
-(ert-deftest test-commander-both-options-and-commands ()
-  (with-mock
-   (mock (x) :times 1)
-   (mock (help "bar") :times 1)
-   (mock (show "qux") :times 1)
-   (mock (command) :times 1)
-   (commander
-    (option "-x" "X" 'x)
-    (option "--show <command>" "SHOW" 'show)
-    (option "--help [command]" "HELP" 'help)
-    (command "command" "COMMAND" 'command)
-    (parse '("-x" "--help" "bar" "--show" "qux" "command")))))
-
 (ert-deftest test-commander-not-greedy-when-single-required-argument ()
   (with-mock
    (mock (foo "bar") :times 1)
