9d9db21f86cc18d6c80a8086362596a902dc3f1c
Johan Andersson <johan.rejeep@gmail.com>
2013-12-29 11:43:57 +0100

    Update usage example.

--- examples/usage.el
+++ examples/usage.el
@@ -8,11 +8,16 @@
 (defun dog (arg)
   (message "The dog says: %s" arg))
 
+(defun help (&optional command-name)
+  (if command-name
+      (commander-print-usage-for-and-exit command-name)
+    (commander-print-usage-and-exit)))
+
 (commander
  (option "--cow <arg>" "Lorem ipsum dolor sit amet" cow)
  (option "--dog [arg]" "Consectetur adipiscing elit. Donec orci dolor" dog "Woff Woff")
- (option "--help, -h" "Luctus sed ullamcorper nec" commander-print-usage)
- (command "help" "Imperdiet eu eros" commander-print-usage)
+ (option "--help [command], -h [command]" "Luctus sed ullamcorper nec" help)
+ (command "help [command]" "Imperdiet eu eros" help)
  (command "foo <bar>" "Duis rhoncus luctus tellus" ignore)
  (command "barbaz [*]" "At porta odio congue id" ignore)
  (command "qux <*>" "Nunc tempus nisl ac erat vulputate vestibulum" ignore))
